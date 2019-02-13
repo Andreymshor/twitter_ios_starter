b69ef682262571882052659e4bf489c79f3f519b
Andrey Shor <andreyshor@andreys-imac.local>
2019-02-13 00:19:34 -0500

    Part 2 of twitter

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -12,47 +12,26 @@ class HomeTableViewController: UITableViewController {
     
     var tweetArray  = [NSDictionary]()
     var numberOfTweet: Int!
-    
-    let myRefreshControl = UIRefreshControl()
+    let Refresher = UIRefreshControl()
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
-    
-        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
-        tableView.refreshControl =  myRefreshControl
+        numberOfTweet = 10
+        
+     
+        loadMoreTweets()
+        
+        
+      
        
     }
-    @objc func loadTweets(){
-        
-        numberOfTweet = 20
-        
+    
+    
+        func loadMoreTweets(){
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": numberOfTweet]
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
-            
-            self.tweetArray.removeAll()
-            for tweet in tweets{
-                
-                self.tweetArray.append(tweet)
-                
-            }
-            
-            self.tableView.reloadData()
-            self.myRefreshControl.endRefreshing()
-            
-            
-        }, failure: { (Error) in
-            print("could not retrive tweets")
-        })
-    }
-    
-    
-    
-    func loadMoreTweets(){
-        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        numberOfTweet = numberOfTweet + 20
-        let myParams = ["count": numberOfTweet]
+        
+        let myParams = ["count": 10]
         
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
             
@@ -64,7 +43,7 @@ class HomeTableViewController: UITableViewController {
             }
             
             self.tableView.reloadData()
-            self.myRefreshControl.endRefreshing()
+           
             
             
         }, failure: { (Error) in
@@ -72,30 +51,9 @@ class HomeTableViewController: UITableViewController {
         })
     }
     
+
     
-    
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
-        if indexPath.row + 1 == tweetArray.count{
-            loadMoreTweets()
-        }
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  
     
     
 
@@ -105,6 +63,7 @@ class HomeTableViewController: UITableViewController {
         
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
     }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
@@ -123,14 +82,13 @@ class HomeTableViewController: UITableViewController {
         }
         
         
-        
-        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
         return cell
     }
     
     
-    
-    
+
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -142,6 +100,7 @@ class HomeTableViewController: UITableViewController {
         return tweetArray.count
     }
 
+    
    
 
 }
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -14,7 +14,51 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
     
-
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    var favorited:Bool = false
+    var tweetId: Int = -1
+    
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        
+        if (toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                 self.setFavorite(true)
+            }, failure: { (error) in
+                print("fav did not succed: \(error)")
+            })
+        } else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("unfav unsuccesful \(error)")
+            })
+        }
+        
+    }
+    
+    
+    @IBAction func retweet(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if (favorited){
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }
+        else {
+            favButton.setImage(UIImage(named: "favor-icon-1"), for: UIControl.State.normal)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
